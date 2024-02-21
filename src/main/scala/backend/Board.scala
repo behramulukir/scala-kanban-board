0d819d6e909817d321c38f76e8893a80e7e02ae1
BehramUlukir <behramulukir@gmail.com>
2024-02-21 16:50:16 +0200

    Adding implementations of KanbanApp and Board classes

---
+++ src/main/scala/backend/Board.scala
@@ -0,0 +1,83 @@
+package backend
+
+import scalafx.scene.image.*
+import java.time._
+import scala.collection.mutable._
+
+class Board(boardName: String, kanbanApp: KanbanApp) {
+  //Creating a random identifier for each board
+  private val random = scala.util.Random
+  val identifier: String = "1/" + LocalDate.now.toString + "/" + random.nextInt(100000).toString // How to assign each item a unique identifier was described detailedly in the technical plan.
+  //Name of the board
+  var name: String = boardName
+  
+  //Lists of subelements of board
+  var allStages: Buffer[Stage] = Buffer()
+  var allCards: Buffer[Card] = Buffer()
+  var allTags: Buffer[Tag] = Buffer()
+  var archivedCards: Buffer[Card] = Buffer()
+  
+  //Background feature for board
+  var background: Option[Image] = None
+  
+  //Adding a new card
+  def addCard(card: Card) =
+    allCards = allCards.addOne(card)
+    card
+  end addCard
+  
+  //Removing a card
+  def removeCard(card: Card): Unit =
+    val indexOfBoard = allCards.indexOf(card)
+    allCards.remove(indexOfBoard)
+  end removeCard
+
+  //Adding a new stage (column)
+  def addStage =
+    val identifier: String = "2/" + LocalDate.now.toString + "/" + random.nextInt(100000).toString
+    val stage = new Stage(this, identifier)
+    allStages = allStages.addOne(stage)
+    stage
+  end addStage
+
+  //Adding a new stage with specified ID
+  def addStageWithID(id: String) = 
+    val stage = new Stage(this, id)
+    allStages = allStages.addOne(stage)
+    stage
+  
+  //Remove a stage
+  def removeStage(stage: Stage) =
+    val indexOfStage = allStages.indexOf(stage)
+    allStages.remove(indexOfStage)
+  end removeStage
+  
+  //Archive a card
+  def archiveCard(cardToArchive: Card) =
+    cardToArchive.archiveCard()
+    archivedCards.addOne(cardToArchive)
+  
+  //De archive a card
+  def dearchiveCard(cardToDearchive: Card) =
+    cardToDearchive.dearchiveCard()  
+    archivedCards.addOne(cardToDearchive)
+  
+  //Filter cards based on the list of tags provided
+  def filter(tags: Buffer[Tag]): Buffer[Card] =
+    var showableCards: Buffer[Card] = Buffer()
+
+    for i <- tags do
+      showableCards = showableCards.addAll(allCards.filter(_.tags.contains(i)))
+    showableCards
+  end filter
+
+  //Change name of the board
+  def changeName(newName: String) = name = newName
+
+  //Change background of the board with given image
+  def changeBackground(image: Image) =
+    background = Some(image)
+  end changeBackground
+
+  
+}
---
+++ src/main/scala/backend/KanbanApp.scala
@@ -0,0 +1,187 @@
+package backend
+
+import scalafx.scene.paint.*
+import scalafx.scene.paint.Color.rgb
+
+import scala.collection.mutable.*
+import scala.io.*
+import java.time.*
+import java.io.*
+import java.awt.*
+import java.time.format.DateTimeFormatter
+
+class KanbanApp:
+  
+  //Lists for all elements
+  var allBoards: Buffer[Board] = Buffer()
+  var allStages: Buffer[Stage] = Buffer()
+  var allCards: Buffer[Card] = Buffer()
+  var allTags: Buffer[Tag] = Buffer()
+  
+  //Last board that user was interacting
+  var lastBoard: Option[Board] = None
+
+  //Adding a board to the app
+  def addBoard(name: String): Board =
+    val board = new Board(name, this)
+    allBoards = allBoards.addOne(board)
+    board
+  end addBoard
+
+  //Function to import from files
+  //Template part is not ready yet
+  def importFunc(file: String) =
+    val board: Board = this.addBoard(file)
+    var stage: Stage = null
+    var card: Card = null
+    var checklistItem: Checklist = null
+
+    try
+      val bufferedSource = Source.fromFile(file)
+      for line <- bufferedSource.getLines do
+        if line.substring(0,5) == "Stag:" then
+          var id = line.drop(5)
+          stage = board.addStageWithID(id)
+        else if line.substring(0,5) == "Card:" then
+          var cardId = line.drop(5)
+          card = stage.addCardWithID(cardId)
+        else if line.substring(0,5) == "Text:" then
+          var textContent = line.drop(5)
+          card.changeDescription(textContent)
+        else if line.substring(0,5) == "Tags:" then
+          var tags = (line.drop(6)).split(" ")
+          for i <- tags do
+            card.tagCreation(i)
+        else if line.substring(0,5) == "Colo:" then
+          var colors = line.drop(5).dropRight(1).split(", ")
+          var red = colors(0).toInt
+          var green = colors(1).toInt
+          var blue = colors(2).toInt
+          card.changeColor(rgb(red, green, blue))
+        else if line.substring(0,5) == "Deli:" then
+          if line.drop(5) != "" then
+            card.changeDeadline(LocalDate.parse(line.drop(5)))
+        else if line.substring(0,5) == "Chck:" then
+          if line.drop(5) != "" then
+            var checks: Array[String] = line.drop(5).split(" - ")
+            var uncompletedSteps: String = checks(1)
+            var completedSteps: String = checks(0)
+            uncompletedSteps = uncompletedSteps.drop(1).dropRight(1)
+            completedSteps = completedSteps.drop(1).dropRight(1)
+            var completedArray = completedSteps.split(", ")
+            var undcompletedArray = uncompletedSteps.split(", ")
+            for i <- completedArray do
+              var checklist = card.checklist.get
+              checklist.addCompletedStep(i)
+            for i <- undcompletedArray do
+              var checklist = card.checklist.get
+              checklist.addUncompletedStep(i)
+        else if line.substring(0,5) == "Acti:" then
+          if line.drop(5) != "" then
+            var activity = line.drop(5).split(", ")
+            card.addInteraction(activity(0).toInt, activity(1))
+        else if line.substring(0,5) == "Clin:" then
+          if line.drop(5) != "" then
+            var cardsAttach = line.drop(5).split(" - ")
+            var attachmentCard = board.allCards.filter(i => i.cardIdentifier == cardsAttach(0))
+            var cardAttach = new cardAttachment(attachmentCard.head)
+            var cardAttachTo = board.allCards.filter(i => i.cardIdentifier == cardsAttach(1))
+            cardAttachTo.head.addAttachment(cardAttach)
+        else if line.substring(0,5) == "Flin:" then
+          if line.drop(5) != "" then 
+            var attachElements = line.drop(5)
+            var fileAttach = new fileAttachment(new File(attachElements))
+            card.addAttachment(fileAttach)
+        else if line.substring(0, 5) == "Arch:" then
+          var archPar = line.drop(5)
+          if archPar == "T" then
+            card.archiveCard()
+        else 
+          println("empty string")
+        bufferedSource.close
+      catch 
+        case fnf: FileNotFoundException => fnf.printStackTrace()
+        case ioe: IOException => ioe.printStackTrace()
+      
+  end importFunc
+
+
+
+  //Export function to export boards to files
+  //Remember to put card attachment to the very end of the text file
+  //Template part is not ready yet
+  def exportFunc(board: Board) =
+    val file = new File(board.name + ".txt")
+
+    val bufferedWriter = new BufferedWriter(new FileWriter(file))
+
+    for stage <- allStages do
+      var stageID = "Stag:" + stage.identifier
+      bufferedWriter.write(stageID)
+      bufferedWriter.newLine()
+      for card <- stage.allCards do
+        var cardID = "Card:" + card.cardIdentifier
+        bufferedWriter.write(cardID)
+        bufferedWriter.newLine()
+        var cardText = "Text:" + card.description
+        bufferedWriter.write(cardText)
+        bufferedWriter.newLine()
+        var tagText = "Tags:"
+        for tag <- card.tags do
+          tagText = tagText + " " + tag.name
+        bufferedWriter.write(tagText)
+        bufferedWriter.newLine()
+        var colorText = "Colo:(" + card.color.getRed.toString + ", " + card.color.getGreen.toString + ", " + card.color.getBlue.toString + ")"
+        bufferedWriter.write(colorText)
+        bufferedWriter.newLine()
+        var deadlineText = "Deli:"
+        if card.deadline.isDefined then
+          deadlineText = deadlineText + card.deadline.get.format(DateTimeFormatter.ISO_LOCAL_DATE)
+        else
+        bufferedWriter.write(deadlineText)
+        bufferedWriter.newLine()
+        var checklistText = "Chck:("
+        var checklistItems = card.checklist.get
+        if card.checklist.nonEmpty then
+          for compStep <- checklistItems.completedSteps do
+            checklistText = checklistText + compStep.description + ", "
+          checklistText = checklistText.dropRight(2) + ") - ("
+          for uncompStep <- checklistItems.uncompletedSteps do
+            checklistText = checklistText + uncompStep.description + ", "
+          checklistText = checklistText.dropRight(2) + ")"
+        else 
+          checklistText = "Chck:"
+        bufferedWriter.write(checklistText)
+        bufferedWriter.newLine()
+        var activityText = "Acti:"
+        if card.interactionInfo != (-1, "") then
+          activityText = activityText + card.interactionInfo(0) + ", " + card.interactionInfo(1)
+        bufferedWriter.write(activityText)
+        bufferedWriter.newLine()
+        var fileAttachmentText = "Flin:"
+        if card.attachments.isEmpty then
+          if card.attachments.get.attachmentType == "File" then
+            var fileAttachment = card.attachments.get
+            fileAttachmentText = fileAttachmentText + fileAttachment.location
+        bufferedWriter.write(fileAttachmentText)
+        bufferedWriter.newLine()
+        var archiveText = "Arch:"
+        if card.archiveParameter then
+          archiveText = archiveText + "T"
+        else
+          archiveText = archiveText + "F"
+        bufferedWriter.write(archiveText)
+        bufferedWriter.newLine()
+
+    for card <- board.allCards do
+      var cardAttachmentText = "Clin:"
+      if card.attachments.nonEmpty then
+        if card.attachments.get.attachmentType == "Card" then
+          var cardAttachment = card.attachments.get
+          cardAttachmentText = cardAttachmentText + card.cardIdentifier + " - " + cardAttachment.cardID
+      bufferedWriter.write(cardAttachmentText)
+      bufferedWriter.newLine()
+
+    bufferedWriter.close()
+
+  end exportFunc
